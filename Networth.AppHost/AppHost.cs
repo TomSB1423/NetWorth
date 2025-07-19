34d77623204f77db272f579f1a3bdee08c8b86c6
Tom Bushell <tsbushell@outlook.com>
2025-07-19 23:31:46 +0100

    Runs with warnings

--- Networth.AppHost/AppHost.cs
+++ Networth.AppHost/AppHost.cs
@@ -3,8 +3,18 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder.AddPostgres("postgres");
 var postgresdb = postgres.AddDatabase("postgresdb");
 
+
 var functions = builder.AddAzureFunctionsProject<Projects.Networth_Backend_Functions>("functions")
     .WithExternalHttpEndpoints()
     .WithReference(postgresdb);
+
+builder.AddNpmApp("react", "../AspireJavaScript.React")
+    .WithReference(functions)
+    .WaitFor(functions)
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
+
 
 builder.Build().Run();
