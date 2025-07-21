d465f89928f91a841f8b7140680d128f5c85bd9c
Tom Bushell <tsbushell@outlook.com>
2025-07-21 21:40:52 +0100

    Application insights

--- Networth.AppHost/AppHost.cs
+++ Networth.AppHost/AppHost.cs
@@ -1,22 +1,25 @@
 using Scalar.Aspire;
 
 var builder = DistributedApplication.CreateBuilder(args);
-
+var insights = builder.AddAzureApplicationInsights("MyApplicationInsights");
 var postgres = builder.AddPostgres("postgres");
 var postgresdb = postgres.AddDatabase("postgresdb");
 
 var functions = builder
     .AddAzureFunctionsProject<Projects.Networth_Backend_Functions>("functions")
     .WithExternalHttpEndpoints()
-    .WithReference(postgresdb);
+    .WithReference(postgresdb)
+    .WithReference(insights);
 
 builder.AddNpmApp("react", "../Networth.Frontend/Networth.Frontend.React")
     .WithReference(functions)
     .WaitFor(functions)
-    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithEnvironment("BACKEND_URL", $"https+http://_dashboard.{functions.Resource.Name}")
     .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints()
-    .PublishAsDockerFile();
+    .PublishAsDockerFile()
+    .WithReference(insights);
+
 
 var scalar = builder.AddScalarApiReference(options =>
 {
